ace69d96cbbf3ceca70cbf42284f5a573b786d55
Omega Melese <omegamelese68@gmail.com>
2025-06-16 15:11:30 +0300

    make the exam room interactive to take exams and show results immediatly using js

--- exam-room.js
+++ exam-room.js
@@ -17,28 +17,31 @@ function renderQuestions(number) {
   const question = questions[number];
 
   const innerHtml = `
+  <div class="timer-container">
+        <p class="timer"></p>
+      </div>
   <div class="question">
         <p class="number">${questionNum + 1},</p>
         <p>${question.question}</p>
       </div>
       <div class="answer">
-        <div class="choice-holder">
-          <input type="radio" data-choice-aplha="A" id="choice-A" name="choice">
+        <div class="choice-holder" data-choice-aplha="A">
+          <input type="radio" id="choice-A" name="choice">
           <label for="choice">${question.choice.A}</label>
         </div>
 
-        <div class="choice-holder">
-          <input type="radio" data-choice-aplha="B" id="choice-B" name="choice">
+        <div class="choice-holder" data-choice-aplha="B">
+          <input type="radio" id="choice-B" name="choice">
           <label for="choice-B">${question.choice.B}</label>
         </div>
 
-        <div class="choice-holder">
-          <input type="radio" data-choice-aplha="C" id="choice-C" name="choice">
+        <div class="choice-holder" data-choice-aplha="C">
+          <input type="radio" id="choice-C" name="choice">
           <label for="choice-C">${question.choice.C}</label>
         </div>
 
-        <div class="choice-holder">
-          <input type="radio" data-choice-aplha="D" id="choice-D" name="choice">
+        <div class="choice-holder" data-choice-aplha="D">
+          <input type="radio" id="choice-D" name="choice">
           <label for="choice-D">${question.choice.D}</label>
         </div>
       </div>
@@ -48,7 +51,7 @@ function renderQuestions(number) {
           ? `<button class="next-button">Next</button>`
             : questionNum + 1 === questions.length
              ? `<button class="previous-button js-previous-button">Previous</button>
-             <button class="previous-button submit">Submit</button>`
+             <button class="previous-button js-submit">Submit</button>`
              : `<button class="previous-button js-previous-button">Previous</button>
              <button class="next-button">Next</button>` }
       </div>`
@@ -56,6 +59,11 @@ function renderQuestions(number) {
   const questionHolder = document.querySelector('.question-holder')
 
   questionHolder.innerHTML = innerHtml;
+
+  if(question.givenAnswer) {
+    const selectedRadio = document.querySelector(`#choice-${question.givenAnswer}`);
+    selectedRadio.checked = true;
+  }
 
   if (!(questionNum + 1 === questions.length)) {
     const nextBtn = document.querySelector('.next-button');nextBtn.addEventListener('click', () => {
@@ -71,6 +79,23 @@ function renderQuestions(number) {
       renderQuestions(questionNum);
     });
   }
+
+  if (questionNum + 1 === questions.length) {
+    const submitBtn = document.querySelector('.js-submit');
+    submitBtn.addEventListener('click', () => {
+      submitAnswer();
+    });
+  }
+
+  const choices = document.querySelectorAll('.choice-holder');
+  choices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+      question.givenAnswer = choice.dataset.choiceAplha;
+
+      const selectedRadio = document.querySelector(`#choice-${choice.dataset.choiceAplha}`);
+      selectedRadio.checked = true;
+    });
+  });
 }
 
 const startBtn = document.querySelector('.start-exam');
@@ -79,7 +104,90 @@ startBtn.addEventListener('click', () => {
   removeWelcome();
   displayQuestion();
   renderQuestions(questionNum);
+  timer();
 });
+
+function submitAnswer() {
+  let innerHTML = `
+      <div class="inner-result">
+        <h1>Result</h1>
+        <div class="score-grade">
+          <p class="score">Score: ${calculateResult()}/${questions.length}</p>
+          <p class="grade">Grade: ${calculateGrade()}</p>
+        </div>
+        <p class="remark">
+          ${calculateRmark()}!
+        </p>
+        <a href=""><button href>Try again</button></a>
+      </div>`;
+
+      const resultcontainer = document.querySelector('.result');
+      resultcontainer.innerHTML = innerHTML;
+
+      displayResult();
+      removeQuestion();
+}
+
+function calculateRmark() {
+  let score = calculateResult();
+
+  if (score >= 0.50 *(questions.length)) {
+    return 'Passed';
+  } else {
+    return 'Faild';
+  }
+}
+
+function calculateGrade() {
+  let score = calculateResult();
+
+  if(score >= 0.85 *(questions.length)) {
+    return 'A';
+  } else if (score >= 0.65 *(questions.length)) {
+    return 'B';
+  } else if (score >= 0.50 *(questions.length)) {
+    return 'C';
+  } else {
+    return 'F';
+  }
+}
+
+function calculateResult() {
+  let score = 0;
+  questions.forEach((question) => {
+    if(question.givenAnswer === question.correctAnswer) {
+      score++;
+    }
+  });
+  return score;
+}
+
+
+function timer() {
+  let timeLeft = 3600; // total time in seconds (2 minutes)
+
+  const countdown = setInterval(() => {
+    const timerElement = document.querySelector(".timer");
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    
+    timerElement.textContent = 
+    `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    
+    timeLeft--;
+
+    console.log(timeLeft);
+    
+    if (timeLeft < 0) {
+      clearInterval(countdown);
+      timerElement.textContent = "Time's up!";
+      setTimeout(() => {
+        /* submitAnswer(); */
+      }, 2000);
+    }
+  }, 1000);
+
+}
 
 function removeWelcome() {
   const welcome = document.querySelector('.welcome-examroom');
@@ -89,4 +197,14 @@ function removeWelcome() {
 function displayQuestion() {
   const questionHolder = document.querySelector('.question-holder');
   questionHolder.style.display = "block";
+}
+
+function removeQuestion() {
+  const questionHolder = document.querySelector('.question-holder');
+  questionHolder.style.display = "none";
+}
+
+function displayResult() {
+  const resultcontainer = document.querySelector('.result');
+  resultcontainer.style.display = "block";
 }
